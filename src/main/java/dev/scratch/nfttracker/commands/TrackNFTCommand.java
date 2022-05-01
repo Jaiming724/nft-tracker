29af8c144cb5aa5793884d2828ff1dcf2534c2ea
Jiaming <Jiamingw29@gmail.com>
2022-05-01 15:18:05 -0400

    NFT tracker will try to send embed of nft

--- src/main/java/dev/scratch/nfttracker/commands/TrackNFTCommand.java
+++ src/main/java/dev/scratch/nfttracker/commands/TrackNFTCommand.java
@@ -3,9 +3,7 @@ package dev.scratch.nfttracker.commands;
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import dev.scratch.nfttracker.model.mongo.Destination;
-import dev.scratch.nfttracker.model.mongo.NFTMongo;
 import dev.scratch.nfttracker.model.mongo.TrackedNFT;
-import dev.scratch.nfttracker.model.nft.NFT;
 import dev.scratch.nfttracker.service.DataService;
 import dev.scratch.nfttracker.service.NFTService;
 import dev.scratch.nfttracker.service.ServerStatsService;
@@ -13,7 +11,6 @@ import dev.scratch.nfttracker.service.TrackedNFTService;
 import org.javacord.api.entity.channel.Channel;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.MessageBuilder;
-import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
 import org.javacord.api.util.logging.ExceptionLogger;
@@ -38,18 +35,29 @@ public class TrackNFTCommand implements CommandExecutor {
         this.trackedNFTService = trackedNFTService;
     }
 
-    @Command(aliases = {"!add"}, description = "Add an NFT to Track", usage = "!add NFT_Name role")
+    @Command(aliases = {"!add"}, description = "Add an NFT to Track", usage = "!add NFT_Name role(optional)")
     public String addNFT(String[] str, Channel channel, Server server, User user) {
         if (!server.isAdmin(user)) {
             return "You need to have admin permissions to run this command";
         }
-        String nftName = str[0];
-        String roleID = str[1];
+        if (str.length < 1 || str.length > 2) {
+            return "Please follow format !add NFT_Name role(optional)";
+        }
+        String nftName;
+        String roleID;
+        if (str.length == 1) {
+            nftName = str[0];
+            roleID = null;
+        } else {
+            nftName = str[0];
+            roleID = str[1];
+        }
+
         logger.debug("Server {} has added {}", server.getIdAsString(), nftName);
         Destination destination = new Destination(roleID, server.getIdAsString(), channel.getIdAsString());
 
         if (trackedNFTService.containNFt(nftName)) {
-            logger.debug("{} already exists in NFT database, adding channel", server.getIdAsString());
+            logger.debug("{} already exists in NFT database, adding channel", nftName);
             trackedNFTService.addDestination(nftName, destination);
             return "Added NFT";
         } else {
@@ -73,6 +81,7 @@ public class TrackNFTCommand implements CommandExecutor {
     private void sendMessage(TrackedNFT nft, Channel channel) {
         if (nft == null) {
             new MessageBuilder().append("That NFT is currently unsupported").send((TextChannel) channel).exceptionally(ExceptionLogger.get());
+
         } else {
             new MessageBuilder().append("Added NFT").send((TextChannel) channel).exceptionally(ExceptionLogger.get());
         }
--- src/main/java/dev/scratch/nfttracker/service/NFTChecker.java
+++ src/main/java/dev/scratch/nfttracker/service/NFTChecker.java
@@ -1,9 +1,11 @@
 package dev.scratch.nfttracker.service;
 
 import dev.scratch.nfttracker.model.mongo.Destination;
+import dev.scratch.nfttracker.model.mongo.NFTMongo;
 import dev.scratch.nfttracker.model.mongo.TrackedNFT;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.Channel;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,7 @@ public class NFTChecker {
     private DiscordApi api;
     private static final Logger logger = LoggerFactory.getLogger(NFTChecker.class);
 
+    @Autowired
     public NFTChecker(TrackedNFTService trackedNFTService, DataService dataService, DiscordApi api) {
         this.trackedNFTService = trackedNFTService;
         this.dataService = dataService;
@@ -46,18 +49,36 @@ public class NFTChecker {
             if (count > nft.getCount()) {
                 logger.info("Detected change for {} from {} to {}", nft.getName(), nft.getCount(), count);
                 trackedNFTService.setCount(nft.getName(), count);
-                for (Destination destination : nft.getServers()) {
-                    logger.info("Sending change for {} to {}", nft.getName(), destination.getChannelID());
-                    String msg = String.format("%s #%d was just minted %s", nft.getName(), count, destination.getRoleID());
+                NFTMongo nftMongo = new NFTMongo();
+                nftMongo.setName(nft.getName());
 
-                    api.getChannelById(destination.getChannelID()).flatMap(Channel::asServerTextChannel).ifPresent(serverTextChannel -> serverTextChannel.sendMessage(msg));
-                }
+                dataService.getImageURL(nft.getName(), nftMongo, count)
+                        .thenAccept(url -> sendEmbed(nft, count, url));
+
             }
         } else {
-            for (Destination destination : nft.getServers()) {
-                api.getChannelById(destination.getChannelID()).flatMap(Channel::asServerTextChannel).ifPresent(serverTextChannel -> serverTextChannel.sendMessage("Supply for NFT was empty. This NFT might no longer be supported"));
+            logger.warn(String.format("Supply for %s was empty. This NFT might no longer be supported", nft.getName()));
+        }
+    }
+
+    public void sendEmbed(TrackedNFT nft, int count, String url) {
+        for (Destination destination : nft.getServers()) {
+            logger.info("Sending change for {} to {}", nft.getName(), destination.getChannelID());
+            String msg;
+            if (destination.getRoleID() == null) {
+                msg = String.format("%s #%d was just minted", nft.getName(), count);
+            } else {
+                msg = String.format("%s #%d was just minted %s", nft.getName(), count, destination.getRoleID());
+            }
+            api.getChannelById(destination.getChannelID()).flatMap(Channel::asServerTextChannel).ifPresent(serverTextChannel -> serverTextChannel.sendMessage(msg));
+            if (url != null) {
+                EmbedBuilder embedBuilder = new EmbedBuilder()
+                        .setTitle(String.format("%s #%d", nft.getName(), count))
+                        .setImage(url);
+                api.getChannelById(destination.getChannelID()).flatMap(Channel::asServerTextChannel).ifPresent(serverTextChannel -> serverTextChannel.sendMessage(embedBuilder));
             }
         }
+
     }
 
 }
